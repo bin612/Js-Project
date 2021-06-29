7a1e9e3b90f4c2cb280f0fdc0f1d74a34fa2e126
kimsangbin <tkdqls612@naver.com>
2021-06-29 15:25:28 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const ajax = new XMLHttpRequest();
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+//동기적으로 처리하겠다 (false)
+ajax.open('GET', 'NEWS_URL',false);
+ajax.send();
+
+//응답 값을 객체로 변환 (json일 경우)
+let newsFead = JSON.parse(ajax.response);
+const ul = document.createElement('ul'); 
+
+for(let i = 0; i < 10; i++){
+    const li = document.createElement('li');
+    li.innerHTML = newsFead[i].title;
+    ul.appendChild(li);
+}
+
+document.getElementById('root').appendChild(ul);
