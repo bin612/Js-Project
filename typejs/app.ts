c5413a7488afb2ad4bb103e024dae5287e2911de
kimsangbin <tkdqls612@naver.com>
2021-07-11 21:09:31 +0900

    2021.07.11/Type

--- typejs/app.ts
+++ typejs/app.ts
@@ -1,13 +1,37 @@
-const container = document.getElementById('root');
-const ajax = new XMLHttpRequest();
+
+type Store = {
+    currentPage: number;
+    feeds: NewsFeed[];
+}
+
+type NewsFeed = {
+    id : number;
+    comment_count: number;
+    url: string;
+    user: string;
+    time_ago: string;
+    points: number;
+    title: string;
+    read?: boolean;
+}
+
+const container: HTMLElement | null = document.getElementById('root');
+const ajax: XMLHttpRequest = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-const store = {
+const store: Store = {
     currentPage : 1,
     feeds: [],
 };
 
+function updateView(html) {
+    if(container){
+        container.innerHTML = html;
+    } else {
+        console.error("최상위 컨테이너가 없어 UI를 진행하지 못했습니다.");
+    }  
+}
 
 //ajax url 처리 및 중복 제거 함수
 function getData(url) {
@@ -28,7 +52,7 @@ function makeFeeds(feeds) {
 //뉴스 리스트 함수
 function newsFeed() {
 
-    let newsFeed = store.feeds;
+    let newsFeed: NewsFeed[] = store.feeds;
     const newsList = [];
     let template = `
             <div class="bg-gray-600 min-h-screen">
@@ -87,9 +111,7 @@ function newsFeed() {
     template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
     template = template.replace('{{__next_page__}}', store.currentPage + 1);
 
-
-    container.innerHTML = template;
-
+    updateView(template);
 }
 
 //뉴스 상세 내용
@@ -157,7 +179,7 @@ function newsDetail(){
         return commentString.join('');
     }
 
-    container.innerHTML = template.replace('{{__comments__}}', makeComment(newsContent.comments));
+   updateView(template.replace('{{__comments__}}', makeComment(newsContent.comments)));
 }
 
 //라우터 : 상황에 맞게 화면을 중계해주는 것 (a화면 b화면 c화면)
