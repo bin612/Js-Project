10fab9375cf36e985e486e4de06d007b648b0ee8
kimsangbin <tkdqls612@naver.com>
2021-07-12 21:13:44 +0900

    2021.07.11/Function/Interface

--- typejs/app.ts
+++ typejs/app.ts
@@ -1,34 +1,35 @@
 
-type Store = {
+interface Store {
     currentPage: number;
     feeds: NewsFeed[];
 }
 
-type News = {
-    id:number;
-    time_ago: string;
-    title: string;
-    url: string;
-    user: string;
-    content: string;
+interface News {
+    readonly id: number;
+    readonly time_ago: string;
+    readonly title: string;
+    readonly url: string;
+    readonly user: string;
+    readonly content: string;
 }
 
 
 // 뉴스피드 형변환
 // 인터섹션 type NewsFeed = News & {}
-type NewsFeed = News & {
-    comments_count: number;
-    points: number;
-    read?: boolean;
+// readonly 지시어라고 말한다. Typescript에서 지원하는 기능 중 하나
+interface NewsFeed extends News {
+    readonly comments_count: number;
+    readonly points: number;
+    read?: boolean; // true/false인 boolean타입이라서 readonly 사용하면 안된다.
 }
 
 //뉴스상세 형변환
-type NewsDetail = News & {
-    comments: NewsComment[];
+interface NewsDetail extends News {
+    readonly comments: NewsComment[];
 }
 
-type NewsComment = News & {
-    comments: NewsComment[];
+interface NewsComment extends News {
+    readonly comments: NewsComment[];
     level: number;
 }
 
